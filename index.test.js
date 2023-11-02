5c9c6b8145214da8e057a87cc33146937f299649
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-11-02 13:22:31 +0000

    tests all sorted

--- index.test.js
+++ index.test.js
@@ -6,7 +6,7 @@ const profiles = require("./seed/profiles.json")
 const likes = require("./seed/likes.json")
 const posts = require("./seed/posts.json")
 
-describe('Social Sequelzie Test', () => {
+describe('Social Sequelize Test', () => {
     /**
      * Runs the code prior to all tests
      */
@@ -25,18 +25,49 @@ describe('Social Sequelzie Test', () => {
     test("testing User to profile relationship 1-1", async () => {
         const firstUser = await User.findOne({where: {id: 1}})
         const firstProfile = await Profile.findOne({where:{id:1}})
-        firstProfile.addUser(firstUser) //addProfile is not a function
+        firstProfile.setUser(firstUser) 
         const firstExpectedUser = await firstProfile.getUser()
         expect(firstExpectedUser).toEqual(firstUser);
     })
-    test("testing User to Post relationship 1-many", function() {
-        expect(true).toBe(true);
+    test("testing User to Post relationship 1-many", async function() { // attempt to assign multiple posts to a user then reading this
+        const firstUser = await User.findOne({where:{id:1}})
+        const firstTwoPosts = await Post.findAll({where:{id:1, id:2}})
+        const firstPost = await Post.findOne({where: {id:1}})
+        const secondPost = await Post.findOne({where:{id:2}})
+        await firstUser.addPosts([firstPost, secondPost])
+        const firstPostAgain = await Post.findOne({where:{id:1}})
+        expect(firstPostAgain.UserId).toBe(1);
     })
-    test("testing Post to Comment relationship 1-many", function() {
-        expect(true).toBe(true);
+    test("testing Post to Comment relationship 1-many", async function() {
+        const firstComment = await Comment.findOne({where:{id:1}})
+        const secondComment = await Comment.findOne({where:{id:2}})
+        const thirdComment = await Comment.findOne({where:{id:3}})
+        const thirdPost = await Post.findOne({where:{id:3}})
+        thirdPost.addComments([firstComment, secondComment, thirdComment])
+        const allComments = await Comment.findAll({where: {PostId:3}})
+        expect(allComments.length).toBe(3);
     })
-    test("testing User to Likes relationship many-many", function() {
-        expect(true).toBe(true);
+    test("testing User to Likes relationship many-many", async function() {
+        const firstUser = await User.findByPk(1)
+        const fourthUser = await User.findByPk(4)
+        const firstLike = await Like.findByPk(1)
+        const secondLike = await Like.findByPk(2)
+        await firstUser.setLikes([firstLike, secondLike])
+        await fourthUser.setLikes([firstLike, secondLike])
+        const firstUserAgain = await User.findByPk(1)
+        const firstUserLikes = await firstUserAgain.getLikes()
+        expect(firstUserLikes.length).toBe(2);
+    })
+    test("testing Likes to Users relationship many-many", async function() {
+        const firstUser = await User.findByPk(1)
+        const fourthUser = await User.findByPk(4)
+        const firstLike = await Like.findByPk(1)
+        const secondLike = await Like.findByPk(2)
+        await firstUser.setLikes([firstLike, secondLike])
+        await fourthUser.setLikes([firstLike, secondLike])
+        const firstLikeAgain = await Like.findByPk(1)
+        const firstLikeUsers = await firstLikeAgain.getUsers()
+        expect(firstLikeUsers.length).toBe(2);
     })
 
 
